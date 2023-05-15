2966f9e336e48d9be58ce3be26b0bab9058817d6
Mateo <mateocarballo2@gmail.com>
2023-05-15 14:14:05 +0200

    Creados varios test del mismo metodo, cada uno en uno de los rangos de trabajo de los metodos/funciones

--- Ejemplos/AJUnit5/src/test/java/es/joseluisgs/dam/OperacionesMatematicasTest.java
+++ Ejemplos/AJUnit5/src/test/java/es/joseluisgs/dam/OperacionesMatematicasTest.java
@@ -1,6 +1,7 @@
 package es.joseluisgs.dam;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OperacionesMatematicasTest {
@@ -26,16 +27,41 @@ class OperacionesMatematicasTest {
     }
 
     @Test
-    void isBisiesto() {
+    @DisplayName("Comprobando valor divisible 4")
+    void isBisiesto1() {
         Assertions.assertTrue(OperacionesMatematicas.isBisiesto(2004));
-        Assertions.assertTrue(OperacionesMatematicas.isBisiesto(1200));
+
+    }
+    @Test
+    @DisplayName("Comprobando valor divisible 4 y 100")
+    void isBisiesto2() {
+
         Assertions.assertTrue(OperacionesMatematicas.isBisiesto(1200));
     }
-
     @Test
-    void isPrimo() {
-        Assertions.assertTrue(!OperacionesMatematicas.isPrimo(1));
-        Assertions.assertTrue(OperacionesMatematicas.isPrimo(3));
-        Assertions.assertTrue(!OperacionesMatematicas.isPrimo(6));
+    @DisplayName("Comprobando valor divisible 4 y 400")
+    void isBisiesto3() {
+
+        Assertions.assertTrue(OperacionesMatematicas.isBisiesto(1200));
+    }
+    @Test
+    @DisplayName("Test 1 menor que cero -100")
+    void isPrimo1() {
+        Assertions.assertFalse(OperacionesMatematicas.isPrimo(-100));
+    }
+    @Test
+    @DisplayName("Test 2 numero 2")
+    void isPrimo2() {
+        Assertions.assertTrue(OperacionesMatematicas.isPrimo(2));
+    }
+    @Test
+    @DisplayName("Test 3 numero 7")
+    void isPrimo3() {
+        Assertions.assertTrue(OperacionesMatematicas.isPrimo(2));
+    }
+    @Test
+    @DisplayName("Test 4 Dentro de valores positivo caso falso")
+    void isPrimo4() {
+        Assertions.assertFalse(OperacionesMatematicas.isPrimo(8));
     }
 }
